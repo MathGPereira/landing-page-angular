2c2e6dfdc39d0243057d94a8e0e1e3e9e33d0fa5
Matheus Pereira <matheusgp.mto@outlook.com>
2023-06-10 21:53:27 -0300

    Criando o template HTML do componente de cabeçalho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
