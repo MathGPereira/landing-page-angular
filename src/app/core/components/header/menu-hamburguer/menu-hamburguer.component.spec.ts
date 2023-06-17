3d66e90cd3915bcc16ecb73384e31e77aabee273
Matheus Pereira <matheusgp.mto@outlook.com>
2023-06-17 00:55:11 -0300

    Iniciando os testes de dom do MenuHamburguerComponent

---
+++ src/app/core/components/header/menu-hamburguer/menu-hamburguer.component.spec.ts
@@ -0,0 +1,57 @@
+import { openNavBar } from 'src/app/shared/animations/menuBars';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MenuHamburguerComponent } from "./menu-hamburguer.component";
+
+describe(MenuHamburguerComponent.name, () => {
+    let fixture!: ComponentFixture<MenuHamburguerComponent>;
+    let component!: MenuHamburguerComponent;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [MenuHamburguerComponent],
+            imports: [BrowserAnimationsModule]
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(MenuHamburguerComponent);
+        component = fixture.componentInstance;
+    });
+
+    it('Should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('Should init property mouseState value when component is created', () => {
+        const mouseState = component.mouseState;
+
+        expect(mouseState.mouseClick).toBeFalse();
+        expect(mouseState.mouseOverOut).toBeFalse();
+    });
+
+    it(`#${MenuHamburguerComponent.prototype.modifyMouseStatus.name}
+        should modify property mouseOverOut value when called`, () => {
+        component.modifyMouseStatus();
+        expect(component.mouseState.mouseOverOut).toBeTrue();
+    });
+
+    it(`#${MenuHamburguerComponent.prototype.modifyClickStatus.name}
+        should modify property mouseClick value and call #openNavBar method to change
+        property (@Output openOff) value when called`, () => {
+        fixture.detectChanges();
+        component.openOff.subscribe(() => {
+            expect(component.mouseState.mouseClick).toBeTrue();
+        });
+        component.modifyClickStatus();
+    });
+
+    it('(D) Should change property mouseOverOut value when mouse click', () => {
+        const menuHamburguer: HTMLDivElement = fixture.nativeElement.querySelector(".hamburguer-menu");
+
+        // menuHamburguer.click();
+        // fixture.detectChanges();
+
+        // expect(component.mouseState.mouseClick).toBeTrue();
+        expect(menuHamburguer).toBeTruthy();
+    });
+});
