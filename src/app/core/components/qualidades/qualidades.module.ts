09fb3e37751c11a040b1cb37e3e84672f08f0043
Matheus Pereira <matheusgp.mto@outlook.com>
2023-06-18 23:06:43 -0300

    Componentizando o formulário

--- src/app/core/components/qualidades/qualidades.module.ts
+++ src/app/core/components/qualidades/qualidades.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { QualidadesComponent } from './qualidades.component';
+import { FormContactComponent } from './form-contact/form-contact.component';
 
 @NgModule({
     declarations: [
-        QualidadesComponent
+        QualidadesComponent,
+        FormContactComponent
     ],
     imports: [
         CommonModule
