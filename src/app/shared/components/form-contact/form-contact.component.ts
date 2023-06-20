ab401916e72c013956eaf46f61a362b1ec6871d7
Matheus Pereira <matheusgp.mto@outlook.com>
2023-06-20 20:07:13 -0300

    Criando a funcionalidade de submitar o formulário

--- src/app/shared/components/form-contact/form-contact.component.ts
+++ src/app/shared/components/form-contact/form-contact.component.ts
@@ -11,7 +11,14 @@ export class FormContactComponent {
     text = 'Submit';
 
     public cleanForm(form: NgForm): void {
-
+        const name = <HTMLInputElement>document.querySelector('#name');
+        const email = <HTMLInputElement>document.querySelector('#email');
+        const phone = <HTMLInputElement>document.querySelector('#phone');
+        if(form.valid) {
+            name.value = '';
+            email.value = '';
+            phone.value = '';
+        }
     }
 
     public contentValidity(input: HTMLInputElement): string {
