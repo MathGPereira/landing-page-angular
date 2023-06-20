720bb4df35e8cc5bf79883ea690b7c73d2683e84
Matheus Pereira <matheusgp.mto@outlook.com>
2023-06-20 19:44:26 -0300

    Criando as validações do formulário com orientação a template

--- src/app/shared/components/form-contact/form-contact.component.ts
+++ src/app/shared/components/form-contact/form-contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-form-contact',
@@ -8,4 +9,8 @@ import { Component } from '@angular/core';
 export class FormContactComponent {
 
     text = 'Submit';
+
+    public cleanForm(form: NgForm): void {
+
+    }
 }
--- src/app/shared/components/form-contact/form-contact.module.ts
+++ src/app/shared/components/form-contact/form-contact.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { FormContactComponent } from './form-contact.component';
 import { ContactButtonModule } from '../contact-button/contact-button.module';
+import { ErrorMessageModule } from '../error-message/error-message.module';
 
 @NgModule({
     declarations: [FormContactComponent],
     imports: [
         ContactButtonModule,
         CommonModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        FormsModule,
+        ErrorMessageModule
     ],
     exports: [FormContactComponent]
 })
