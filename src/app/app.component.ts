e929de0b90e73909664cafbac1051eb1e8e6ba3b
Matheus Pereira <matheusgp.mto@outlook.com>
2023-06-20 22:21:46 -0300

    Criando o template e o HTML da sessão de howWeDoItComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +9,11 @@ export class AppComponent {
 
     title = 'Designing & Building';
     text = 'Contact us';
+
+    firstExample = {
+        title: 'Meet and define goals',
+        image: 'image1',
+        firstDescription: 'The first thing we do is meeting with our clients and talk through their goals on a future project. During this meeting, feel free to communicate your ideas and ask lots of questions.',
+        secondDescription: 'This stage is highly decisive as you can evaluate the work of your potential architect by browsing their portfolio. As a client, you may also assess whether the architect listens to your needs and confirms that he or she understands them.'
+    }
 }
