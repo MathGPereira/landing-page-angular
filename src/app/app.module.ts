3b05d9506215a7b7405c5a3620702dcc0e115f02
Matheus Pereira <matheusgp.mto@outlook.com>
2023-06-20 21:08:23 -0300

    Criando o HowWeDoComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,17 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderModule } from './core/components/header/header.module';
-import { ContactsComponent } from './core/components/contacts/contacts.component';
 import { CarouselComponent } from './core/components/carousel/carousel.component';
 import { CarouselModule } from './core/components/carousel/carousel.module';
 import { ContactButtonModule } from './shared/components/contact-button/contact-button.module';
 import { QualidadesModule } from './core/components/qualidades/qualidades.module';
+import { HowWeDoComponent } from './core/components/how-we-do/how-we-do.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        ContactsComponent,
-        CarouselComponent
+        CarouselComponent,
+        HowWeDoComponent
     ],
     imports: [
         BrowserModule,
---
+++ src/app/core/components/how-we-do/how-we-do.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-how-we-do',
+  templateUrl: './how-we-do.component.html',
+  styleUrls: ['./how-we-do.component.scss']
+})
+export class HowWeDoComponent {
+
+}
